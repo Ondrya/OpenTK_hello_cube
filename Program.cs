a244f569f0851f91b75eb1e9957eb225b5851049
Andrew <ondrya@yandex.ru>
2019-07-30 20:20:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK_Tutorial_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTK_Tutorial_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game())
+            {
+                game.Run(30, 30);
+            }
+        }
+    }
+}
